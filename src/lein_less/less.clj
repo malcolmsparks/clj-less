dd43bda48927c59b539768da2832db437219bbbc
tangrammer <juanantonioruz@gmail.com>
2014-12-01 10:48:19 +0100

    WIP

--- src/lein_less/less.clj
+++ src/lein_less/less.clj
@@ -2,12 +2,43 @@
   (:require
    [lein-less.less.compiler :as compiler]
    [lein-less.less.engine :as engine]
-   [lein-less.less.nio :as nio])
-  (:import (lein_less.less LessError)))
+   [lein-less.less.nio :as nio]
+   [clojure.java.io :as io])
+  (:import [lein_less.less LessError]
+           [java.io File]))
 
 (defn- report-error [^LessError error]
   (binding [*out* *err*]
     (println (.getMessage error))))
+
+
+(defn up-to-date?
+  "returns true if src is up to date with long last-modification"
+  [^File src ^Long last-modification]
+  (>= (.lastModified (io/file src)) last-modification))
+
+(defn synchronized?
+  [^File target ^File source]
+  (up-to-date? source (.lastModified (io/file target))))
+
+
+(defn synchronized-folder-source?
+  "return the more recent time modification (greater lastModified value) of folder files.
+  Returns long type"
+  [source-folder time-target]
+  (loop [d (next (file-seq source-folder))]
+    (let [f (first d)]
+      (if (.isDirectory f)
+        (recur (next (file-seq  f)))
+        (if (up-to-date? f time-target)
+          (if (next d)
+            (recur (next d))
+            true)
+          false)))))
+
+(defn is-synchronized? [src-folder target-path]
+  (synchronized-folder-source? (io/file src-folder) (.lastModified (io/file target-path))))
+
 
 (defn run-compiler
   "Run the lesscss compiler."
--- test/lein_less/changes_checker_test.clj
+++ test/lein_less/changes_checker_test.clj
@@ -1,21 +1,11 @@
 (ns lein-less.changes-checker-test
   (:require [clojure.java.io :as io]
             [clojure.test :refer :all]
+            [lein-less.less :as less]
             [clojure.repl :refer (apropos dir doc find-doc pst source)])
   (:import [java.io File]))
 
 
-(defn up-to-date?
-  "returns true if targer has same last modified date as source"
-  [^File src ^Long last-modification]
-  ;;  (println [(.lastModified (io/file source)) (.lastModified (io/file target))])
-  (>= (.lastModified (io/file src)) last-modification))
-
-(defn synchronized?
-  "returns true if targer has same last modified date as source"
-  [^File target ^File source]
-  ;;  (println [(.lastModified (io/file source)) (.lastModified (io/file target))])
-  (up-to-date? source (.lastModified (io/file target))))
 
 
 (def directory (clojure.java.io/file "sample"))
@@ -24,28 +14,8 @@
 
 (.isDirectory(first (file-seq (io/file "sample/example"))))
 
-(assert (complement (synchronized? "sample/.DS_Store" "sample/example/project.clj")))
-(assert (synchronized? "sample/example/less/sample.less" "sample/example/project.clj"))
+(assert (complement (less/synchronized? "sample/.DS_Store" "sample/example/project.clj")))
+(assert (less/synchronized? "sample/example/less/sample.less" "sample/example/project.clj"))
 
-
-(defn synchronized-folder-source?
-  "return the more recent time modification (greater lastModified value) of folder files.
-  Returns long type"
-  [source-folder time-target]
-  (loop [d (next (file-seq source-folder))]
-    (let [f (first d)]
-      (if (.isDirectory f)
-        (do
-          (println f)
-          (recur (next (file-seq  f))))
-        (if (up-to-date? f time-target)
-          (if (next d)
-            (recur (next d))
-            true)
-          false)))))
-
-(defn is-synchronized? [src-folder target-path]
-  (synchronized-folder-source? (io/file src-folder) (.lastModified (io/file target-path))))
-
-(is-synchronized? "sample/example" "sample/.DS_Store" )
-(is-synchronized? "sample/example" "sample/example/project.clj")
+(assert (complement  (less/is-synchronized? "sample/example" "sample/.DS_Store" )))
+(assert(less/is-synchronized? "sample/example" "sample/example/project.clj"))
