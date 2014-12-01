53f168957bffdbcb33dc5a62b4ecff1b38d4ed52
tangrammer <juanantonioruz@gmail.com>
2014-12-01 10:41:34 +0100

    WIP: Add check last modified time functions

--- test/lein_less/changes_checker_test.clj
+++ test/lein_less/changes_checker_test.clj
@@ -1,41 +1,51 @@
 (ns lein-less.changes-checker-test
   (:require [clojure.java.io :as io]
             [clojure.test :refer :all]
-            [clojure.repl :refer (apropos dir doc find-doc pst source)]))
+            [clojure.repl :refer (apropos dir doc find-doc pst source)])
+  (:import [java.io File]))
 
 
-(defn synchronized-with-source?
+(defn up-to-date?
   "returns true if targer has same last modified date as source"
-  [target source]
-  (println [(.lastModified (io/file source)) (.lastModified (io/file target))])
-  (>= (.lastModified (io/file target)) (.lastModified (io/file source)) ))
+  [^File src ^Long last-modification]
+  ;;  (println [(.lastModified (io/file source)) (.lastModified (io/file target))])
+  (>= (.lastModified (io/file src)) last-modification))
+
+(defn synchronized?
+  "returns true if targer has same last modified date as source"
+  [^File target ^File source]
+  ;;  (println [(.lastModified (io/file source)) (.lastModified (io/file target))])
+  (up-to-date? source (.lastModified (io/file target))))
 
 
 (def directory (clojure.java.io/file "sample"))
 (def files (file-seq directory))
 
 
+(.isDirectory(first (file-seq (io/file "sample/example"))))
+
+(assert (complement (synchronized? "sample/.DS_Store" "sample/example/project.clj")))
+(assert (synchronized? "sample/example/less/sample.less" "sample/example/project.clj"))
+
+
 (defn synchronized-folder-source?
-  "return the more recent time modification (greater lastModified value) of folder files. Returns long type"
+  "return the more recent time modification (greater lastModified value) of folder files.
+  Returns long type"
   [source-folder time-target]
-  (let [d (io/file source-folder) ]
-    (doseq [f (.listFiles d)]
+  (loop [d (next (file-seq source-folder))]
+    (let [f (first d)]
       (if (.isDirectory f)
         (do
-          ;; (println "dir: " (str source-folder "/"(.getName f)))
-          (synchronized-folder-source? (str f "/" (str source-folder "/"(.getName f))) time-target))
-        (do
-          (println "file: " (str source-folder "/"(.getName f)))
-          (when )
-            )
-        ))))
+          (println f)
+          (recur (next (file-seq  f))))
+        (if (up-to-date? f time-target)
+          (if (next d)
+            (recur (next d))
+            true)
+          false)))))
 
 (defn is-synchronized? [src-folder target-path]
-  (synchronized-folder-source? src-folder (.lastModified (io/file target-path))))
+  (synchronized-folder-source? (io/file src-folder) (.lastModified (io/file target-path))))
 
 (is-synchronized? "sample/example" "sample/.DS_Store" )
-
-
-
-(assert (synchronized-with-source? "sample/.DS_Store" "sample/example/project.clj"))
-(assert (synchronized-with-source? "sample/example/less/sample.less" "sample/example/project.clj"))
+(is-synchronized? "sample/example" "sample/example/project.clj")
